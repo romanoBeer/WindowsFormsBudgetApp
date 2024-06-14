1a5ee0bb031417b2b1630512eb22d005c3ad0831
romanoBeer <99826861+romanoBeer@users.noreply.github.com>
2024-06-14 11:42:10 +0200

    Add project files.

---
+++ WindowsFormsApp1/Login.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class loginFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginFrm));
+            this.registerButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.pswdField = new System.Windows.Forms.TextBox();
+            this.usrnameField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // registerButton
+            // 
+            this.registerButton.ForeColor = System.Drawing.Color.Black;
+            this.registerButton.Location = new System.Drawing.Point(132, 135);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(104, 23);
+            this.registerButton.TabIndex = 4;
+            this.registerButton.Text = "Register";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.registerButton_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.ForeColor = System.Drawing.Color.Black;
+            this.loginButton.Location = new System.Drawing.Point(25, 135);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(101, 23);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // pswdField
+            // 
+            this.pswdField.Location = new System.Drawing.Point(25, 93);
+            this.pswdField.Name = "pswdField";
+            this.pswdField.Size = new System.Drawing.Size(211, 20);
+            this.pswdField.TabIndex = 2;
+            // 
+            // usrnameField
+            // 
+            this.usrnameField.Location = new System.Drawing.Point(25, 38);
+            this.usrnameField.Name = "usrnameField";
+            this.usrnameField.Size = new System.Drawing.Size(211, 20);
+            this.usrnameField.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // loginFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(261, 193);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.usrnameField);
+            this.Controls.Add(this.pswdField);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.registerButton);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "loginFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.loginFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox pswdField;
+        private System.Windows.Forms.TextBox usrnameField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Login.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class loginFrm : Form
+    {
+        public loginFrm()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection conn = new SqlConnection(@"Data Source=ROMANO\SQLEXPRESS;Initial Catalog=loginTest;Integrated Security=True;Encrypt=False;");
+
+        private void loginFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String querry = "SELECT * FROM login WHERE username = '"+usrnameField.Text+"' AND password = '"+pswdField.Text+"'";
+                SqlDataAdapter sda = new SqlDataAdapter(querry, conn);
+
+                DataTable dataTable = new DataTable();
+                sda.Fill(dataTable);
+
+                if (dataTable.Rows.Count > 0)
+                {
+                    Dashboard dashboard = new Dashboard();
+                    dashboard.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Login, Try agian!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    usrnameField.Clear();
+                    pswdField.Clear();
+
+                    usrnameField.Focus();
+                }
+            }
+            catch 
+            {
+                MessageBox.Show("Error!", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally 
+            {
+                conn.Close();
+            }
+        }
+
+        private void registerButton_Click(object sender, EventArgs e)
+        {
+            Register register = new Register();
+            register.Show();
+            this.Hide();
+        }
+    }
+}
